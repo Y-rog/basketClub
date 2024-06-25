4663f44107d04b370fbd3aaada9a4bdad299313a
Y-rog <fulgueiras.gregory@gmail.com>
2024-06-25 14:53:48 +0200

    feat: Adding home and contact component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'bcf-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
